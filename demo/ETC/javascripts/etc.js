7ac4991bce259caeb1da075a14c3d621e9f1226d
Pulipuli Chen <pulipuli.chen@gmail.com>
2016-06-16 04:01:37 +0800

    0615 我不行了QQ

--- demo/ETC/javascripts/etc.js
+++ demo/ETC/javascripts/etc.js
@@ -46,6 +46,8 @@
 //		}, 500);
 //	}
 //});
+
+
 /*
 	########   #######  ##       ########
 	##     ## ##     ## ##       ##     ##
@@ -116,7 +118,6 @@ $("document").ready(function() {
 
 $(function () {
     
-    
     setTimeout(function () {
         $("div.contain label.hc-name").each(function (_index, _element) {
             //console.log(1);
@@ -139,21 +140,38 @@ $(function () {
     var _timer_mouseleave = {};
     
     var _div = $("div.contain > div");
-    var _mouseover_delay = 1000;
-    var _mouseleave_delay = 5000;
+    var _mouseover_delay = 5000;
+    var _mouseleave_delay = 3500;
+    var _blur_delay = 2000;
     
-    _div.mouseover(function () {
+    var _prev_line = true;
+    
+    var _height = window.innerHeight;
+    var doc = document.documentElement;
+    
+    _div.mouseover(function (_event) {
         var _this = $(this);
+        var _current_top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+        //console.trace([_event.pageY - _current_top, _this.offset().top - _current_top, _height]);
+        if (_prev_line === true && _this.prev().length === 1 && (_this.offset().top- _current_top) > (_height / 3)) {
+            _this = _this.prev();
+        } 
         var _index = _this.parent().children().index(_this);
         _index = "div" + _index;
         //console.log(_index);
         if (_this.attr("enter") === "true") {
             return;
         }
+        //_disable_blur();
         
         //clearTimeout(_timer_mouseenter[_index]);
         //clearTimeout(_timer_mouseleave[_index]);
         _this.attr("enter", "true");
+        
+        setTimeout(function () {
+            _enable_blur(_this);
+        }, _blur_delay);
+        
         _timer_mouseenter[_index] = setTimeout(function () {
             //clearTimeout(_timer_mouseenter[_index]);
             //clearTimeout(_timer_mouseleave[_index]);
@@ -165,6 +183,7 @@ $(function () {
             
             //_this.find("label").mouseenter();
             _this.find(".hc-details").addClass("hovercard-display");
+            //_disable_blur();
             _this.attr("hovercard_display", "true");
             
             //_this.attr("enter", "false");
@@ -183,6 +202,7 @@ $(function () {
             _this.attr("leave", "false");
             _this.find(".hc-details").removeClass("hovercard-display");
             _this.attr("hovercard_display", "false");
+            //_disable_blur();
         }
         else {
             _timer_mouseleave[_index] = setTimeout(function () {
@@ -193,8 +213,39 @@ $(function () {
     
     _div.mouseout(function () {
         var _this = $(this);
+        var _current_top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+        if (_prev_line === true && _this.prev().length === 1 && (_this.offset().top- _current_top) > (_height / 3)) {
+            _this = _this.prev();
+        } 
         _this.attr("enter", "false");
     });
+    
+    var _blur_enabled = false;
+    
+    var _enable_blur = function (_this) {
+        //if (_blur_enabled === true) {
+        //    return;
+        //}
+        
+        $(".line > span").addClass("blur");
+        $(".line > div.hc-preview").addClass("blur");
+        _this.find(".blur").removeClass("blur");
+        var _next_line = _this.next();
+        if (_next_line.length > 0) {
+            _next_line.find(".blur").removeClass("blur");
+        }
+        var _prev_line = _this.prev();
+        if (_prev_line.length > 0) {
+            _prev_line.find(".blur").removeClass("blur");
+        }
+        
+        _blur_enabled = true;
+    };
+    
+    _disable_blur = function () {
+        $(".blur").removeClass("blur");
+        _blur_enabled = false;
+    };
     
 //    _div.mouseout(function () {
 //        
@@ -216,4 +267,160 @@ $(function () {
 //        }, 2000);
 //        
 //    });
+});
+
+// ------------------------------------------------------
+// 換頁功能
+
+// 先插入兩個位置吧
+$(function () {
+    var _page_turner_up = $('<div class="page-turner up"></div>').appendTo("body");
+    var _page_turner_down = $('<div class="page-turner down"></div>').appendTo("body");
+    
+    var doc = document.documentElement;
+    var _height = window.innerHeight;
+    var body = document.body,
+    html = document.documentElement;
+
+    var _body_height = Math.max( body.scrollHeight, body.offsetHeight, 
+                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+    
+    var _page_timer = {
+        "pageup":undefined,
+        "pagedown":undefined
+    };
+    var _turner_delay = 3000;
+    var _remove_hover_delay = 3000;
+    var _turner_height = 400;
+    
+    _page_turner_up.mouseover(function () {
+        var _this = $(this);
+        if (_this.attr("hover") === "true") {
+            return;
+        }
+        
+        _this.attr("hover", "true");
+        _this.addClass("hover");
+        clearTimeout(_page_timer["pageup"]);
+        _page_timer["pageup"] = setTimeout(function () {
+            //clearTimeout(_page_timer["pageup"]);
+            if (_this.attr("hover") !== "true") {
+                _this.removeClass("hover");
+                return;
+            }
+            var _top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+            var _to_top = _top - (_height - _turner_height);
+            if (_to_top < 0) {
+                _to_top = 0;
+                _this.addClass("disable");
+            }
+            _disable_blur();
+            _scrollTo(document.body, _to_top, 1000);
+            _this.attr("scrolling", "true");
+            setTimeout(function () {
+                _this.attr("hover", "false");
+                _this.attr("scrolling", "false");
+            }, _remove_hover_delay);
+            _this.removeClass("hover");
+            
+            _page_turner_down.removeClass("disable");
+        }, _turner_delay);
+    });
+    
+    _page_turner_up.mouseout(function () {
+        var _this = $(this);
+        if (_this.attr("scrolling") === "true") {
+            return;
+        }
+        _this.removeClass("hover");
+        _this.attr("hover", "false");
+        //clearTimeout(_page_timer["pageup"]);
+    });
+    
+    _page_turner_down.mouseover(function () {
+        var _this = $(this);
+        if (_this.attr("hover") === "true") {
+            return;
+        }
+        
+        _this.attr("hover", "true");
+        _this.addClass("hover");
+        clearTimeout(_page_timer["pagedown"]);
+        _page_timer["pagedown"] = setTimeout(function () {
+            clearTimeout(_page_timer["pagedown"]);
+            if (_this.attr("hover") !== "true") {
+                _this.removeClass("hover");
+                return;
+            }
+            var _top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+            var _to_top = _top + (_height - _turner_height);
+            if (_to_top + _height > _body_height - 1) {
+                console.trace(["_page_turner_down", _to_top, _height, _to_top+_height, _body_height]);
+                _this.addClass("disable");
+            }
+            _disable_blur();
+            _this.attr("scrolling", "true");
+            _scrollTo(document.body, _to_top, 1000);
+            setTimeout(function () {
+                _this.attr("hover", "false");
+                _this.attr("scrolling", "false");
+                clearTimeout(_page_timer["pagedown"]);
+            }, _remove_hover_delay);
+            _this.removeClass("hover");
+            _page_turner_up.removeClass("disable");
+        }, _turner_delay);
+    });
+    
+    _page_turner_down.mouseout(function () {
+        var _this = $(this);
+        if (_this.attr("scrolling") === "true") {
+            return;
+        }
+        _this.removeClass("hover");
+        _this.attr("hover", "false");
+        //clearTimeout(_page_timer["pagedown"]);
+    });
+    
+    var _scrollTo = function(element, to, duration, _callback) {
+        if (duration <= 0) {
+            if (typeof(_callback) === "function") {
+                _callback();
+            }
+            return;
+        }
+        var difference = to - element.scrollTop;
+        var perTick = difference / duration * 10;
+
+        setTimeout(function() {
+            element.scrollTop = element.scrollTop + perTick;
+            if (element.scrollTop === to) return;
+            _scrollTo(element, to, duration - 10, _callback);
+        }, 10);
+    };
+    
+    // ------------------------
+    // 確認初始位置，來決定是否要disable
+    var _check_page_turner_disable = function () {
+        var _current_top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+        //console.trace([_current_top, _height, _current_top + _height, _body_height]);
+        if (_page_turner_up.attr("scrolling") !== "true") {
+            if (_current_top === 0) {
+                _page_turner_up.addClass("disable");
+            }
+            else {
+                _page_turner_up.removeClass("disable");
+            }
+        }
+        
+        if (_current_top + _height > _body_height - 1) {
+            _page_turner_down.addClass("disable");
+        }
+        else if (_page_turner_down.attr("scrolling") !== "true") {
+            _page_turner_down.removeClass("disable");
+        }
+    };
+    _check_page_turner_disable();
+    $(window).scroll(_check_page_turner_disable);
+    
+    
 });
